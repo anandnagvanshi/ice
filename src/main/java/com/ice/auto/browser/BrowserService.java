2d8e46dd8e529ce36cc8839f85862ad5c9c96834
anandnagvanshi <60606616+anandnagvanshi@users.noreply.github.com>
2021-05-03 15:55:25 +1000

    Update BrowserService.java

--- src/main/java/com/ice/auto/browser/BrowserService.java
+++ src/main/java/com/ice/auto/browser/BrowserService.java
@@ -2,27 +2,40 @@ package com.ice.auto.browser;
 
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Singleton class to hold Browser instance
+ */
 public class BrowserService {
+    private static BrowserService instance= null;
+    private static ThreadLocal<WebDriver> webDriverThreadLocal =new ThreadLocal<>();
 
-	private static BrowserService instance=null;
-	private static WebDriver webDriver;
-	
-	private BrowserService() {
-		webDriver=BrowserController.selectBrowser(System.getProperty("browser").toUpperCase());
-	}
-	
-	public static BrowserService getInstance() {
-		
-		if(instance == null){
+
+    private BrowserService(){
+//        setThreadLoacalWebDriver();
+
+    }
+
+    public void setBrowserType(String browserType) {
+        webDriverThreadLocal.set(BrowserController.selectBrowser(browserType));
+    }
+
+    /**
+     * To get the instance of BrowserService
+     * @return BrowserService
+     */
+    public static BrowserService getInstance() {
+        if (instance == null) {
             instance = new BrowserService();
         }
         return instance;
-		
-	}
-	
-	public WebDriver getBrowser() {
-		
-		return webDriver;
-	}
+    }
+
+    /**
+     * To get the Browser Instance
+     * @return Webdriver
+     */
+    public WebDriver getBrowser(){
+        return webDriverThreadLocal.get();
+    }
 }
 
